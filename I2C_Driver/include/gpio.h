5acbaecbb42fee0cf62ce56317ee105bafc41454
vishal-95-um <vishalchhokra9@gmail.com>
2026-07-16 21:29:36 +0530

    Update I2C driver comments

--- I2C_Driver/include/gpio.h
+++ I2C_Driver/include/gpio.h
@@ -4,7 +4,7 @@
 
 void gpio_driver_init(); // Initialie G21 and G22 for I2C ie make both open drain and make both high for idle
 
-void gpio_release(uint8_t pin); // 
+void gpio_release(uint8_t pin); 
 
 void gpio_pull_low(uint8_t pin);
 
--- I2C_Driver/include/i2c.h
+++ I2C_Driver/include/i2c.h
@@ -13,7 +13,7 @@ bool i2c_get_ack(); // Get ACK from slave
 bool i2c_write_byte(uint8_t data);
 uint8_t i2c_read_byte(bool ack);
 
-void i2c_send_ack();
-void i2c_send_nack();
+void i2c_send_ack(); // Send ACK to Slave (Write 0 on SDA Line)
+void i2c_send_nack(); // Send NACK to Slave (Write 1 on SDA Line)
 
-void i2c_stop();
+void i2c_stop(); 
